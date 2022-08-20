d470a7a4be2bed8c4100713835221d1494497e12
SowndharyaSekaran <ssowndharyact@gmail.com>
2022-08-20 16:11:33 +0530

    Amazon page added

---
+++ Amazon/src/test/java/org/amazon/page/AmazonPage.java
@@ -0,0 +1,14 @@
+package org.amazon.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonPage {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+
+	}
+
+}

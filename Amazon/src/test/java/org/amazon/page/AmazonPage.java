babc80e04df99727109952574f8541120e6d74a7
SowndharyaSekaran <ssowndharyact@gmail.com>
2022-08-30 09:36:28 +0530

    Added WebDriverManager in line # 12 with importing in up of the code

--- Amazon/src/test/java/org/amazon/page/AmazonPage.java
+++ Amazon/src/test/java/org/amazon/page/AmazonPage.java
@@ -1,18 +1,20 @@
 package org.amazon.page;
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class AmazonPage {
-
-	public static void main(String[] args) {
+public class AmazonPage {	
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().getPageLoadTimeout();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("mouse pad");
-		driver.findElement(By.id("nav-search-submit-button")).click();
 		
-
-	}
-
+	}		
 }

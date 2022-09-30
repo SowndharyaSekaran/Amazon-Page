d9306a2b54afcf6a2eb44b49519d94712a95fc23
SowndharyaSekaran <ssowndharyact@gmail.com>
2022-09-30 14:18:48 +0530

    Changes done using windows handling

--- Amazon/src/test/java/org/amazon/page/AmazonPage.java
+++ Amazon/src/test/java/org/amazon/page/AmazonPage.java
@@ -1,10 +1,13 @@
 package org.amazon.page;
 import java.time.Duration;
+import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AmazonPage {	
@@ -17,10 +20,17 @@ public class AmazonPage {
 		driver.manage().window().maximize();
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("mouse pad");
 		driver.findElement(By.id("nav-search-submit-button")).click();
-		driver.findElement(By.linkText("Tizum Z65- Mouse Pad for Laptop, Notebook, MacBook Pro Air, Gaming Computer. 9.4 * 7.9 Inches, Anti-Skid Base Mousepad (MP4)")).click();
-		driver.get("https://www.amazon.in/Notebook-MacBook-Computer-Anti-Skid-Mousepad/dp/B08QJJCY2Q/ref=sr_1_3?keywords=mouse+pad&qid=1661794607&sr=8-3");
-		String mainwindow = driver.getWindowHandle();		
-		driver.switchTo().window(mainwindow);
+		String mainwindow = driver.getWindowHandle();
+
+		driver.findElement(By.xpath("//html/body/div/div[2]/div/div/div/span[3]/div[2]/div[7]/div/div/div/div/div/div[2]/div/div/div/h2/a/span")).click();
+		Set<String> windows = driver.getWindowHandles();
+		String child=null;
+		for(String a:windows) {
+			if(!a.equalsIgnoreCase(mainwindow)) {
+				child=a;
+			}
+		}
+		driver.switchTo().window(child);
 		WebElement we=driver.findElement(By.name("quantity"));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		Select s=new Select(we);
@@ -28,8 +38,20 @@ public class AmazonPage {
 		driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
 		driver.findElement(By.xpath("//*[@id=\"sw-gtc\"]/span/a")).click();
 		driver.findElement(By.className("a-color-link")).click();
-		driver.get("https://www.amazon.in/gp/product/B08QJJCY2Q/ref=ox_sc_act_title_delete_1?smid=A14CZOWI0VEHLG&psc=1");
-		driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+		WebElement we1 =driver.findElement(By.xpath("//html/body/div/div[2]/div[3]/div[4]/div/div[2]/div/div/form/div[2]/div/div/div/span/a"));
+
+		Actions a = new Actions(driver);
+		a.moveToElement(we1).click().perform();
+
+
+		Set<String> allWindows = driver.getWindowHandles();
+		for(String currentWindow : allWindows){
+			driver.switchTo().window(currentWindow);
+		}
+
+		WebElement we2 = driver.findElement(By.id("add-to-cart-button"));
+		we2.click();
+
 		driver.findElement(By.name("proceedToRetailCheckout")).click();
 		driver.findElement(By.id("createAccountSubmit")).click();
 		driver.findElement(By.id("ap_customer_name")).sendKeys("Sowndharya");
@@ -37,6 +59,6 @@ public class AmazonPage {
 		driver.findElement(By.id("ap_email")).sendKeys("sow@gmail.com");
 		driver.findElement(By.id("ap_password")).sendKeys("sow@123");
 		driver.quit();
-		
+
 	}		
 }
